8a054b4f291b80b32d8583384f87dc5fdd625371
Tomas Korenko <radixxko@gmail.com>
2018-11-16 15:07:52 +0100

    Version 0.0.1

---
+++ test/test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Logger = require('../lib/logger.js');
+
+const logger = new Logger();
+
+//logger.on( 'log', log => console.log, )
+
+logger.attachLoggerStream( Logger.level.notice, ( log ) => { console.log( log.format() ); } );
+logger.attachLoggerStream( Logger.level.devel, { stream: process.stdout, colors: true });
+
+
+logger.emergency({ tags: ['test', 'test2'] }, 'test');
+logger.alert({ tags: ['test', 'test2'] }, 'test');
+logger.notice({ tags: ['test', 'test2'] }, 'test');
+logger.notice({ tags: ['test', 'test2'] }, 'test');
+logger.warning({ tags: ['test', 'test2'] }, 'test');
+logger.notice({ tags: ['test', 'test2'] }, 'test');
+logger.notice({ tags: ['test', 'test2'] }, 'test');
+logger.critical('Pana', 'beka', { error: 'dasda' });
+logger.debug({ tags: ['test', 'test2'] }, 'test');
+logger.devel('Pana', 'beka', { error: 'dasda' });
+
+logger.info({ tags: ['test', 'test2'] }, 'test');
